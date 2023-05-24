c6694d9aa52711712b870aaa751917875a4f687b
Kevin George Dalpathadu <kevin.dalpathadu95@gmail.com>
2023-05-25 01:42:14 +0200

    Created a crud application using SpringBoit

---
+++ src/main/java/com/example/myCrud/MyCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.myCrud;
+//tutorial Link https://www.youtube.com/watch?v=IucFDX3RO9U&t=176s
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/myCrud/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.myCrud.repository;
+
+import com.example.myCrud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
